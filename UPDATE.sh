98bb73ca9f683329f5bcee1c1bb353f10bf55672
ARez2 <dark.gaming@fantasymail.de>
2025-06-21 13:07:25 +0200

    add update script

--- UPDATE.sh
+++ UPDATE.sh
@@ -7,7 +7,12 @@ git fetch upstream
 git merge upstream/master master
 git push --force
 git checkout arez2/changes
+git pull --recurse-submodules
 git rebase master
+cd modules/limboai
+git pull upstream support-godot-4-5
+cd ..
+cd ..
 git push --force
 
 scons platform=linuxbsd target=editor precision=double
